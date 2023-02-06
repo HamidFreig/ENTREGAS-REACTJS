4ebdbe6a269bfdd6ea395e9835ec17b822b9187d
HamidFreig <h.morafreig@uandresbello.edu>
2023-02-06 17:50:51 -0300

    Primera pre-entrega

---
+++ DC1/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/FuncionalComponents/NavBarComponent/NavBar";
+import ItemListContainer from "./components/FuncionalComponents/ItemListContainer/ItemListContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ DC1/src/components/FuncionalComponents/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,20 @@
+import "./ItemListContainerStyle.css";
+
+export default function MenuComponent() {
+  return (
+    <div className="MenuFlex">
+      <div className="MenuComponent">
+        <ul>
+          <li onClick={alertMessagge}>POLERAS</li>
+          <li onClick={alertMessagge}>POLERONES</li>
+          <li onClick={alertMessagge}>ZAPATILLAS</li>
+          <li onClick={alertMessagge}>JOCKEYS</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function alertMessagge() {
+  alert("¡Pronto nuevas funciones!");
+}
